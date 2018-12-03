b0a0b02e29ccb78cba497f8d04ced0145b46b01b
Konoplev Vladimir Andreevich <v.konoplev@tinkoff.ru>
2018-12-03 10:29:30 +0300

    Format fixes

--- src/caseRunner.ts
+++ src/caseRunner.ts
@@ -1,41 +1,41 @@
-import { Case } from './models/case';
-import { CaseType } from './enums/caseType';
+import {Case} from './models/case';
+import {CaseType} from './enums/caseType';
 
 export class CaseRunner {
-  constructor(private name: string, private test: Function) {}
+	constructor(private name: string, private test: Function) {}
 
-  runCases(cases: Case[]) {
-    cases.forEach(item => {
-      this.runCase(item);
-    });
-  }
+	runCases(cases: Case[]) {
+		cases.forEach(item => {
+			this.runCase(item);
+		});
+	}
 
-  private runCase(item: Case) {
-    const expectation = this.prepareNameByArgs(this.name, item.data);
-    const assertion = () => {
-      this.test.call(null, item);
-    };
+	private runCase(item: Case) {
+		const expectation = this.prepareNameByArgs(this.name, item.data);
+		const assertion = () => {
+			this.test.call(null, item);
+		};
 
-    switch (item.type) {
-      case CaseType.Normal:
-        it(expectation, assertion);
-        break;
-      case CaseType.Focused:
-        fit(expectation, assertion);
-        break;
-      case CaseType.Excluded:
-        xit(expectation, assertion);
-        break;
-      default:
-        throw new Error('Wrong case type.');
-    }
-  }
+		switch (item.type) {
+			case CaseType.Normal:
+				it(expectation, assertion);
+				break;
+			case CaseType.Focused:
+				fit(expectation, assertion);
+				break;
+			case CaseType.Excluded:
+				xit(expectation, assertion);
+				break;
+			default:
+				throw new Error('Wrong case type.');
+		}
+	}
 
-  private prepareNameByArgs(name: string, args: any[]): string {
-    return name.replace(/(\$\d+)/g, match => {
-      const index = parseFloat(match.substr(1)) - 1;
+	private prepareNameByArgs(name: string, args: any[]): string {
+		return name.replace(/(\$\d+)/g, match => {
+			const index = parseFloat(match.substr(1)) - 1;
 
-      return args[index].toString();
-    });
-  }
+			return args[index].toString();
+		});
+	}
 }
--- src/models/case.ts
+++ src/models/case.ts
@@ -1,6 +1,6 @@
-import { CaseType } from '../enums/caseType';
-import { CaseData } from './caseData';
+import {CaseType} from '../enums/caseType';
+import {CaseData} from './caseData';
 
 export class Case {
-  constructor(public type: CaseType, public data: CaseData) {}
+	constructor(public type: CaseType, public data: CaseData) {}
 }
